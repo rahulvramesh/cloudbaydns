842d669bd3712d5545e37c18285971de5bb7fc46
Rahul V Ramesh <rahulvramesh@hotmail.com>
2022-03-05 21:49:50 +0530

    Removed Database Init From Main, Since which will be done by alambic

--- app/main.py
+++ app/main.py
@@ -6,12 +6,6 @@ from app.core.config import settings
 from .dependencies import get_query_token
 from .routers import items, users
 
-from .database import models
-from .database.database import engine
-
-# Meta Data creation
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
