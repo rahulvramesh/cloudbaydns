bc60a844f0c864ab625c518db086e46fe0d0d167
Rahul V Ramesh <rahulvramesh@hotmail.com>
2022-03-05 19:33:51 +0530

    Database

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,15 @@
-from pydantic import BaseSettings
+from typing import List
+
+from pydantic import AnyHttpUrl, BaseSettings
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     class Config:
         case_sensitive = True
--- app/main.py
+++ app/main.py
@@ -1,17 +1,46 @@
-from fastapi import FastAPI, Depends
-
-from .dependencies import get_query_token, get_token_header
-from .routers import items, users
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
 from app.core.config import settings
 
+from .dependencies import get_query_token
+from .routers import items, users
+
+from .database import crud, models, schemas
+from .database.database import SessionLocal, engine
+
+# Meta Data creation
+models.Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    dependencies=[Depends(get_query_token)]
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    dependencies=[Depends(get_query_token)],
 )
 
-app.include_router(users.router)
-app.include_router(items.router)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(users.router, prefix=settings.API_V1_STR)
+app.include_router(items.router, prefix=settings.API_V1_STR)
 
 
 @app.get("/")
